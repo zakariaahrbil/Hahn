b7cb8ba6687f717ea846422e8ae2c89b0142a16b
zakaria ahrbil <zakariaahr@gmail.com>
2025-12-21 03:50:43 +0100

    refactor NavBar component to conditionally render links based on authentication status

--- taski_frontend/src/components/navBar.tsx
+++ taski_frontend/src/components/navBar.tsx
@@ -1,13 +1,23 @@
-import { LogIn } from "lucide-react";
+import { LogIn, LucideLayoutDashboard } from "lucide-react";
 import { CustomLink } from "./customLink";
+import { useAuth } from "@/auth/authContext";
 
 export const NavBar = () => {
+  const { isAuthenticated } = useAuth();
+
   return (
     <nav className="w-full flex justify-between bg-white/9 backdrop-blur-md items-center py-4 px-8 mt-4 rounded-2xl shadow-xl">
       <span className="text-2xl font-bold text-white italic">Taski</span>
-      <CustomLink link={{ pathname: "/login" }}>
-        <LogIn className="h-5 w-5" /> Login
-      </CustomLink>
+      {!isAuthenticated ? (
+        <CustomLink link={{ pathname: "/login" }}>
+          <LogIn className="h-5 w-5" /> Login
+        </CustomLink>
+      ) : (
+        <CustomLink link={{ pathname: "/projects" }}>
+          <LucideLayoutDashboard className="h-5 w-5" />
+          Dashboard
+        </CustomLink>
+      )}
     </nav>
   );
 };
